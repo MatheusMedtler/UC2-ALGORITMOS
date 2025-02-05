f9477520e191eeeace9999b579aebcf075a6a06d
Matheus Medtler Alves <ma284163@gmail.com>
2025-02-05 20:55:20 -0300

    Add files via upload

---
+++ 05-02/OperadoresAritmeticos.js
@@ -0,0 +1,36 @@
+//Soma
+const primeiroValor = 10
+const segundoVAlor = 20
+
+const resultado = primeiroValor + segundoVAlor + 20
+
+console.log(resultado)
+
+
+//Exercicios
+
+//1 
+const valor = 3
+const valor2 = 4
+const resultadoo = valor2 + valor
+
+console.log(resultadoo)
+
+//2
+const val = 3
+const valr = 5
+const multiplicacao = val * valr / 2
+console.log(multiplicacao)
+
+//3
+
+const sub = 5
+const subs = 4
+const subtrair = sub - subs 
+const res = subtrair * -1
+console.log(subtrair)
+
+//4
+
+let resto = 234 % 5
+console.log(resto)
